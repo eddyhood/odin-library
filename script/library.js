2ce8721b70deafdd71a82d6666bd61b8f755a0c1
eddyhood <eddy@ignitespot.com>
2022-05-09 22:58:10 -0600

    Redo script to use local storage correctly
    Delete script.js and replace with library.js which has the correct get, set, and remove functions built in for local storage. remove
    spaghetti code.

---
+++ script/library.js
@@ -0,0 +1,123 @@
+//Logic to change form display------------------------------
+const form = document.getElementById('pop-up');
+
+//Add
+const seeFormBtn = document.getElementById('add');
+seeFormBtn.addEventListener('click', displayForm);
+
+function displayForm() {
+    form.style.display = 'block';
+};
+
+//Hide
+const hideFormBtn = document.getElementById('cross');
+hideFormBtn.addEventListener('click', hideForm);
+
+function hideForm() {
+    form.style.display = 'none';
+};
+
+
+//logic to delete local storage-------------------------------
+const eraseLibraryBtn = document.getElementById('empty');
+eraseLibraryBtn.addEventListener('click', clearStorage);
+function clearStorage() {
+    window.localStorage.clear();
+};
+
+//Logic to get user input-------------------------------------
+form.addEventListener('submit', (e) => getBookData(e));
+
+function getBookData(e) {
+    // e.preventDefault();
+    let title = (e.target.elements['title'].value);
+    let author = (e.target.elements['author'].value);
+    let pages = (e.target.elements['pages'].value);
+    let rating = (e.target.elements['rating'].value);
+    let book = new CreateBook(title, author, pages, rating);
+    storeToMemory(title, book);
+};
+
+//Logic to create books----------------------------------------
+function CreateBook(title, author, pages, rating){
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.rating = rating;
+};
+
+//Logic to manage local storage--------------------------------
+
+function storeToMemory(key, book){
+    window.localStorage.setItem(key, JSON.stringify(book));
+};
+
+
+//Logic to display library
+const libraryDisplay = document.getElementById('card-main');
+
+function showBooks() {
+    const main = document.getElementById('card-main');
+    const keys = Object.keys(localStorage);
+    let i = 0;
+
+    for(let key of keys){
+        let book = JSON.parse(window.localStorage.getItem(key));
+
+        const card = document.createElement('div');
+
+         //delete button
+         const deleteBtn = document.createElement('i');
+         deleteBtn.classList.add('flaticon-cross');
+         card.appendChild(deleteBtn);
+         
+         //title
+         const titleStyle = document.createElement('h3');
+         titleStyle.classList.add('book-title')
+         const titleName = document.createTextNode(book.title);
+         titleStyle.appendChild(titleName);
+         card.appendChild(titleStyle);
+         
+         //author
+         const authorStyle = document.createElement('p');
+         const authorName = document.createTextNode('Author Name: ' + book.author);
+         authorStyle.appendChild(authorName);
+         card.appendChild(authorStyle);
+         
+         //pages
+         const pagesStyle = document.createElement('p');
+         const pagesName = document.createTextNode('Page Count: ' + book.pages);
+         pagesStyle.appendChild(pagesName);
+         card.appendChild(pagesName);
+         
+         //Rating
+         const ratingStyle = document.createElement('p');
+         const ratingName = document.createTextNode('Rating: ' + book.rating);
+         ratingStyle.appendChild(ratingName);
+         card.appendChild(ratingStyle);
+
+          //Position
+          const position = i;
+          const addPosition = card.setAttribute('id', i);
+          i++;
+
+        card.classList.add('libraryCard');
+        main.appendChild(card);
+
+    };
+ 
+};
+
+showBooks()
+
+
+//Logic to delete individual books------------------------
+const deleteBook = document.querySelectorAll('.flaticon-cross');
+
+
+deleteBook.forEach(deleteBook => {
+    deleteBook.addEventListener('click', () => {
+        const key = deleteBook.nextSibling.innerText;
+        window.localStorage.removeItem(key);
+    });
+});
